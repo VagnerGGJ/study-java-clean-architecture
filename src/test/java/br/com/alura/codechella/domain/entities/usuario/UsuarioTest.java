7b73986c12d8fbb77ea395c82441794f9dd9b6ae
Vagner Gois <vagnerg.gois@gmail.com>
2025-08-19 09:53:52 -0300

    Criando domain de Usuário.

---
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "Jaque", LocalDate.parse("1990-09-08"), "emailemail.com"));
+    }
+}
